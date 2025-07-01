4ca2c5d0307bd1096d2e30369d1d7df1fda33374
Sharp0802 <sharp0802.business@gmail.com>
2025-07-01 16:00:19 +0900

    :sparkles: Implement concrete classes of IO abstractions for named pipe

---
+++ wsac-service/WsACService/InterProcess/Pipe/PipeLowLevelReader.cs
@@ -0,0 +1,32 @@
+using System.IO.Pipes;
+using WsACService.InterProcess.Abstractions;
+using WsACService.Memory;
+using WsACService.Net;
+
+namespace WsACService.InterProcess.Pipe;
+
+public class PipeLowLevelReader(PipeStream pipe) : ILowLevelReader
+{
+    private readonly MemoryBuffer _buffer = new();
+
+    public async Task ReadAsync(MemoryBuffer buffer, long size, CancellationToken ct)
+    {
+        buffer.Reserve(size);
+        await pipe.ReadAsync(buffer, size, ct);
+    }
+
+    public void Read(Span<byte> buffer, CancellationToken ct)
+    {
+        pipe.Read(buffer, ct);
+    }
+
+    public T Read<T>(CancellationToken ct) where T : unmanaged
+    {
+        return pipe.Read<T>(ct);
+    }
+
+    public Task SkipAsync(long size, CancellationToken ct)
+    {
+        return pipe.SkipAsync(_buffer, size, ct);
+    }
+}
---
+++ wsac-service/WsACService/InterProcess/Pipe/PipeLowLevelWriter.cs
@@ -0,0 +1,27 @@
+using System.IO.Pipes;
+using WsACService.InterProcess.Abstractions;
+using WsACService.Memory;
+
+namespace WsACService.InterProcess.Pipe;
+
+public class PipeLowLevelWriter(PipeStream pipe) : ILowLevelWriter
+{
+    public void Write(Span<byte> buffer)
+    {
+        pipe.Write(buffer);
+    }
+
+    public void Write<T>(T value) where T : unmanaged
+    {
+        unsafe
+        {
+            Span<byte> buffer = stackalloc byte[sizeof(T)];
+            pipe.Write(buffer);
+        }
+    }
+
+    public Task WriteAsync(MemoryBuffer buffer, long size, CancellationToken ct)
+    {
+        return pipe.WriteAsync(buffer, size, ct);
+    }
+}
